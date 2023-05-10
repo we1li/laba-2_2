3926345acf1aff097712d19b5517582610526250
we1li <125406661+we1li@users.noreply.github.com>
2023-05-10 14:02:34 +0300

    idz1.py

--- idz1.py
+++ idz1.py
@@ -1,11 +1,15 @@
-n = int(input("Число купленных карандашей: "))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-if 0 <= n <= 10:
-    if n == 0 or n >= 5:
-        print("Я купил {0} карандашей".format(n))
-    elif n == 1:
-        print("Я купил {0} карандаш".format(n))
-    elif n == 2 or n == 3 or n == 4:
-        print("Я купил {0} карандаша".format(n))
-else:
-    print("Число карандашей может быть в диапазоне от 0 до 10")
+if __name__ == '__main__':
+    n = int(input("Число купленных карандашей: "))
+
+    if 0 <= n <= 10:
+        if n == 0 or n >= 5:
+            print("Я купил {0} карандашей".format(n))
+        elif n == 1:
+            print("Я купил {0} карандаш".format(n))
+        elif n == 2 or n == 3 or n == 4:
+            print("Я купил {0} карандаша".format(n))
+    else:
+        print("Число карандашей может быть в диапазоне от 0 до 10")
