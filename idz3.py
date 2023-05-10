eff975204595a9032741584249828dc9658fb9eb
we1li <125406661+we1li@users.noreply.github.com>
2023-05-10 14:03:23 +0300

    Update idz3.py

--- idz3.py
+++ idz3.py
@@ -1,9 +1,13 @@
-r = range(100)
-for b in r:
-    for k in r:
-        for t in r:
-            if b + k + t == 100 and (10 * b) + (5 * k) + (t * 0.5) == 100:
-                print("Быков:", b)
-                print("Коров:", k)
-                print("Телят:", t)
-                print(" ")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    r = range(100)
+    for b in r:
+        for k in r:
+            for t in r:
+                if b + k + t == 100 and (10 * b) + (5 * k) + (t * 0.5) == 100:
+                    print("Быков:", b)
+                    print("Коров:", k)
+                    print("Телят:", t)
+                    print(" ")
